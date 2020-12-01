9128fdd4dd9161de324d1937840a36ecd19ceaa6
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-01 10:34:08 +0100

    tdjnd

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-var i = 15;
-while (i   ) {
+let i = 15;
+while (i < 45) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+   i++;
 }
-
-
